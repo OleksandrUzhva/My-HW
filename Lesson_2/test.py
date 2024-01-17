f388ddb6d5aa0cf9c64b9a5e81db00fc18699cc2
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-01-17 17:24:55 +0100

    add tabulation

--- Lesson_2/test.py
+++ Lesson_2/test.py
@@ -1,10 +1,13 @@
 class person:
     def __init__(self, name, age) -> None:
-        self.name = name 
+        self.name = name
         self.age = age
+
 
 class price:
     def __init__(self) -> None:
         pass
+
+
 hw = "hello world"
 print(hw)
