d0f135912d7c32262b4cab35faa5cd5f89afc143
OleksandrUzhva <oleksandruzhva@gmail.com>
2024-01-17 16:11:24 +0100

    price class is added

---
+++ Lesson_2/test.py
@@ -0,0 +1,10 @@
+class person:
+    def __init__(self, name, age) -> None:
+        self.name = name 
+        self.age = age
+
+class price:
+    def __init__(self) -> None:
+        pass
+hw = "hello world"
+print(hw)
